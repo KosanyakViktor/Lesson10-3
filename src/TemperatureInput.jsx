a0c683700d1c75b2adfb5dc844a121f7892015fd
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-02-01 17:29:44 +0200

    coma

--- src/TemperatureInput.jsx
+++ src/TemperatureInput.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BoilingVerdict = props => {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit',
 };
 
-export default BoilingVerdict;
+class TemperatureInput extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = (e) => {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;
